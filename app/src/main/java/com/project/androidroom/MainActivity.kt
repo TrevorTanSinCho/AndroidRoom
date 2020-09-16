5c73eafa67f2a10c97b95a258de4a48900f7d675
TrevorTanSinCho <xingzhu89@gmail.com>
2020-09-17 00:41:58 +0800

    implement ListAdapter

--- app/src/main/java/com/project/androidroom/MainActivity.kt
+++ app/src/main/java/com/project/androidroom/MainActivity.kt
@@ -8,9 +8,12 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +24,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        val bookListAdapter = BookListAdapter(this)
+        recyclerview.adapter = bookListAdapter
+        recyclerview.layoutManager = LinearLayoutManager(this)
 
         fab.setOnClickListener{ view ->
             val intent = Intent(this, NewBookActivity::class.java)
