3be01993ab4c16485bafcdacd3c81fc0fb20b12f
TrevorTanSinCho <xingzhu89@gmail.com>
2020-09-17 00:04:56 +0800

    implement Database insert

--- app/src/main/java/com/project/androidroom/MainActivity.kt
+++ app/src/main/java/com/project/androidroom/MainActivity.kt
@@ -1,38 +1,55 @@
 package com.project.androidroom
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var bookViewModel: BookViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener{ view ->
+            val intent = Intent(this, NewBookActivity::class.java)
+            startActivityForResult(intent, NEW_NOTE_ACTIVITY_REQUEST_CODE)
+        }
+
+        bookViewModel = ViewModelProviders.of(this).get(BookViewModel::class.java)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == NEW_NOTE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            val id = UUID.randomUUID().toString()//generated a random UUID
+            val authorName = data?.getStringExtra(NewBookActivity.NEW_AUTHOR)
+            val bookName = data?.getStringExtra(NewBookActivity.NEW_BOOK)
+
+            if (authorName != null && bookName != null){
+                val book = Book(id, authorName, bookName)
+                bookViewModel.insert(book)
+            }
+
+            Toast.makeText(applicationContext,R.string.saved,Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(applicationContext, R.string.not_saved, Toast.LENGTH_LONG).show()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    companion object {
+        private const val NEW_NOTE_ACTIVITY_REQUEST_CODE = 1
     }
 }
